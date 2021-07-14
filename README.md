5d01ecf56b5554c03b6d41e41dcc9a20f6326359
fanleisong <fanleisong@163.com>
2021-07-14 11:08:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# sqleditor_vue
+
+一个SQL 编辑器的前端界面
+将之前的sqlEditor用vue重写一遍
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
+
+### Run your tests
+```
+yarn run test
+```
+
+### Lints and fixes files
+```
+yarn run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/GetDll.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      这是一个调用第三方应用的链接
+      <a href="nav:" target="_blank" rel="noopener">Vue to a new APP</a>.
+    </p>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GetDll',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
